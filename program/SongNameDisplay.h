3939a38f3c402730e7435e01c85a2f11266b78e6
Re:N <nok133otuy@gmail.com>
2023-10-10 20:58:50 +0900

    曲名表示を折り返し入退場するように変更しました フォント実装後に時間などの調整を行う予定です

--- program/SongNameDisplay.h
+++ program/SongNameDisplay.h
@@ -6,12 +6,17 @@ using std::wstring;
 
 class SongNameDisplay {
 private:
+	double pos_x;
+	double arg;
+	double scroll_speed;
 	int generated_clock;
+	double last_updated_clock;
 	wstring song_name;
-	static unsigned int POS_X;
-	static unsigned int POS_Y;
-	static unsigned int SCROLL_SPEED;
-	static unsigned int DURATION;
+	static const unsigned int INIT_POS_X;
+	static const unsigned int POS_Y;
+	static const double INIT_ARG;
+	static const double INIT_SCROLL_SPEED;
+	// static unsigned int DURATION;
 public:
 	SongNameDisplay(wstring given_song_name);
 	SongNameDisplay() = default;
