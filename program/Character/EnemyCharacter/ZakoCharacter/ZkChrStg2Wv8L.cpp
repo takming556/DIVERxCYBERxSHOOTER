8b6de7b16d56a05147f07788782830c2e7004827
KONTACT406 <nok133otuy@gmail.com>
2023-10-12 00:59:45 +0900

    Stage2Wave8の当たり判定拡大とスプライトの拡大を行いました

--- program/Character/EnemyCharacter/ZakoCharacter/ZkChrStg2Wv8L.cpp
+++ program/Character/EnemyCharacter/ZakoCharacter/ZkChrStg2Wv8L.cpp
@@ -17,11 +17,11 @@ const double ZkChrStg2Wv8L::INIT_POS_X = InFieldPosition::MIN_MOVABLE_BOUNDARY_X
 const double ZkChrStg2Wv8L::INIT_POS_Y = InFieldPosition::MIN_MOVABLE_BOUNDARY_Y + 100;
 const double ZkChrStg2Wv8L::INIT_SPEED = 100;
 const double ZkChrStg2Wv8L::INIT_ARG = 0.0 * pi;
-const unsigned int ZkChrStg2Wv8L::COLLIDANT_SIZE = 20;
+const unsigned int ZkChrStg2Wv8L::INIT_COLLIDANT_SIZE = 20;
 const unsigned int ZkChrStg2Wv8L::INIT_HP = 10;
 const unsigned int ZkChrStg2Wv8L::LINE_UP_Y = InFieldPosition::MAX_MOVABLE_BOUNDARY_Y - 130;
-
-const double ZkChrStg2Wv8L::DRAW_EXTRATE = 0.05;
+const double ZkChrStg2Wv8L::COLLIDANT_EXPANDED_RATE = 10.0;
+const double ZkChrStg2Wv8L::INIT_DRAW_EXTRATE = 0.05;
 
 Stg2WAVE8WaitFlag ZkChrStg2Wv8L::WAIT_FLAG_L18 = Stg2WAVE8WaitFlag::WAIT;
 Stg2WAVE8WaitFlag ZkChrStg2Wv8L::WAIT_FLAG_L27 = Stg2WAVE8WaitFlag::WAIT;
@@ -34,16 +34,18 @@ ZkChrStg2Wv8L::ZkChrStg2Wv8L(enum CharacterID given_id, int generated_id, double
 		INIT_POS_X,
 		INIT_POS_Y,
 		INIT_HP,
-		make_unique<CollideCircle>(INIT_POS_X, INIT_POS_Y, COLLIDANT_SIZE)
+		make_unique<CollideCircle>(INIT_POS_X, INIT_POS_Y, INIT_COLLIDANT_SIZE)
 	),
 	line_up_x(0),
 	speed(INIT_SPEED),
 	lower_down_speed(lower_speed),
 	arg(INIT_ARG),
+	collidant_radius(INIT_COLLIDANT_SIZE),
 	last_updated_clock(DxLib::GetNowHiPerformanceCount()),
 	last_generated_clock(DxLib::GetNowCount()),
 	move_status(Stg2WAVE8MoveFlag::RAISE),
-	order_num(generated_id)
+	order_num(generated_id),
+	draw_extrate(INIT_DRAW_EXTRATE)
 {
 	line_up_x = InFieldPosition::MAX_MOVABLE_BOUNDARY_X / 9.0 * generated_id;
 }
@@ -80,6 +82,9 @@ void ZkChrStg2Wv8L::update() {
 	else if (move_status == Stg2WAVE8MoveFlag::LOWER) {
 		arg = 3.0 / 2.0 * pi;
 		speed = lower_down_speed;
+		collidant_radius += COLLIDANT_EXPANDED_RATE * update_delta_time / 1000 / 1000;
+		draw_extrate += 0.15 / 75.0 * COLLIDANT_EXPANDED_RATE * update_delta_time / 1000 / 1000;
+		collidant->set_radius(collidant_radius);
 	}
 
 	double distance = speed * update_delta_time / 1000 / 1000;
@@ -94,6 +99,6 @@ void ZkChrStg2Wv8L::update() {
 
 void ZkChrStg2Wv8L::draw() {
 	Position draw_pos = position->get_draw_position();
-	DxLib::DrawRotaGraph(draw_pos.x, draw_pos.y, DRAW_EXTRATE, 0, ImageHandles::SPRITE_ZKCHR_LIGHT_ELE, TRUE, TRUE);
+	DxLib::DrawRotaGraph(draw_pos.x, draw_pos.y, draw_extrate, 0, ImageHandles::SPRITE_ZKCHR_LIGHT_ELE, TRUE, TRUE);
 	if (DebugParams::DEBUG_FLAG == true) collidant->draw();
 }
