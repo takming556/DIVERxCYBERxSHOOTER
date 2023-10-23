5b1340841a6516102b0ccf2e3a2fd70fbd20353d
dolpast <teroru720phi@gmail.com>
2023-10-23 09:00:58 +0900

    マージ後に発生していた軽微な不整合を修正しました。

--- program/Offensive/Bullet/StraightShot/StraightShot.cpp
+++ program/Offensive/Bullet/StraightShot/StraightShot.cpp
@@ -66,14 +66,6 @@ void StraightShot::draw() {
 		DxLib::DrawRotaGraph(draw_pos.x , draw_pos.y , 0.50 , -arg , ImageHandles::BUBBLE_AQUA , TRUE);
 		break;
 
-	case SkinID::STG3_WAVE1_R:
-		DxLib::DrawRotaGraph(draw_pos.x,draw_pos.y,0.75,-arg,ImageHandles::BUBBLE_BLACK,TRUE);
-		break;
-
-	case SkinID::STG3_WAVE1_L:
-		DxLib::DrawRotaGraph(draw_pos.x,draw_pos.y,0.75,-arg,ImageHandles::BUBBLE_BLACK,TRUE);
-		break;
-
 	case SkinID::STG2_WAVE5_L:
 		DxLib::DrawRotaGraph(draw_pos.x, draw_pos.y, 0.75, -arg, ImageHandles::ANCHOR_AQUA, TRUE);
 		break;
